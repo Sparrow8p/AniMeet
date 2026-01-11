c342aa33ea33dfb8922610e638017881bb131005
Алексей Никонов <aleksey.nikonov.4@gmail.com>
2026-01-12 01:01:43 +0500

    Update for deploy

--- app/settings.py
+++ app/settings.py
@@ -10,20 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parentf
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*!tu@7*h7h@c)%xogrhf54q47_j)w*ug7giif5l(gwe+6+f)gr'
+SECRET_KEY = 'django-insecure-*!tu@7*h7ojgfpoaejfopwekh@c)%xogrhf54q47_gfssgsj)w*ug7giif5l(gwge+6+f)gr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Sparrow8p.pythonanywhere.com', '127.0.0.1']
 
@@ -126,6 +127,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 
