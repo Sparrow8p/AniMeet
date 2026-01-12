76f580a53b2f7f98e920ea7e8f47beb6f2a76fc9
Алексей Никонов <aleksey.nikonov.4@gmail.com>
2026-01-12 23:41:29 +0500

    Update wsgi

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,16 +1,20 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+os.environ['DJANGO_SECRET_KEY'] = 'ваш_секретный_ключ_50+_символов'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 
 from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
+# 1. Путь к проекту
+path = '/home/Sparrow8p/AniMeet'
+if path not in sys.path:
+    sys.path.append(path)
+
+# 2. Файл настроек
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
