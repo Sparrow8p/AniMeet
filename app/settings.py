03d2da2bfad5f750c23b2272c8f034f940062e14
Алексей Никонов <aleksey.nikonov.4@gmail.com>
2026-01-12 23:22:01 +0500

    Secret_key fixed

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("DJANGO_SECRET_KEY не задан! Установите переменную окружения на продакшене.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
