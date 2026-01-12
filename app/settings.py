3e3ec30db6a2a2af07024f2dc3546bf9eaea1632
Алексей Никонов <aleksey.nikonov.4@gmail.com>
2026-01-12 23:16:00 +0500

    Update base_dir and domen

--- app/settings.py
+++ app/settings.py
@@ -17,8 +17,7 @@ from pathlib import Path
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -29,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['Sparrow8p.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['sparrow8p.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
